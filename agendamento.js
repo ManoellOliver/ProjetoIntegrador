1d8c53b1703b0604cbc1072d2da331e5274c8fd0
ManoellOliver <manoeloliver081@gmail.com>
2023-11-01 01:07:44 -0300

    atualizando o projeto

--- agendamento.js
+++ agendamento.js
@@ -26,3 +26,23 @@ function submitForm() {
         alert('Agendamento bem-sucedido! Computador selecionado: ' + selectedPC.id);
     }
 }
+
+function selectComputer(computerId) {
+    // Mostra a seleção de data e hora quando um computador é selecionado
+    document.getElementById('date-time-selection').style.display = 'block';
+
+    // Configura a data mínima (hoje) e a data máxima (7 dias úteis a partir de hoje)
+    var dataMin = new Date();
+    var dataMax = new Date();
+    var diasUteis = 0;
+    
+    while (diasUteis < 7) {
+        dataMax.setDate(dataMax.getDate() + 1);
+        if (dataMax.getDay() !== 0 && dataMax.getDay() !== 6) {
+            diasUteis++;
+        }
+    }
+
+    document.getElementById('data').min = dataMin.toISOString().split('T')[0];
+    document.getElementById('data').max = dataMax.toISOString().split('T')[0];
+}
