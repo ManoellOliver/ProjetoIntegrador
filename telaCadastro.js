f20a9d21bdbe1d4cbc817ecd3550a30af452768e
ManoellOliver <manoeloliver081@gmail.com>
2023-11-25 00:49:44 -0300

    atualizando o css do projeto

--- telaCadastro.js
+++ telaCadastro.js
@@ -1,23 +1,19 @@
+// JavaScript - telaCadastro.js
 document.addEventListener("DOMContentLoaded", function () {
   const form = document.getElementById("registration-form");
   const successMessage = document.getElementById("success-message");
 
   form.addEventListener("submit", function (event) {
-    event.preventDefault();
+      event.preventDefault();
 
-    const name = document.getElementById("name").value;
-    const email = document.getElementById("email").value;
-    const password = document.getElementById("password").value;
-    const confirmPassword = document.getElementById("confirm-password").value;
+      const name = document.getElementById("name").value;
+      const email = document.getElementById("email").value;
+      const password = document.getElementById("password").value;
 
-    if (password !== confirmPassword) {
-      document.getElementById("confirm-password-error").textContent =
-        "As senhas não coincidem.";
-    } else {
-      document.getElementById("confirm-password-error").textContent = "";
+      // Restante do código...
+
       // Simulação de sucesso de cadastro
       successMessage.style.display = "block";
       form.reset();
-    }
   });
 });
